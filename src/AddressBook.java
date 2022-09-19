fd0ebb55bc3ee0abc128a413b2f5309a57bd6a99
yasmi <yasminayounes@cmail.carleton.ca>
2022-09-19 14:54:48 -0400

    With the string output

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AddressBook {
+    private ArrayList<BuddyInfo> BuddyList; //Array list of buddyInfo Objects
+
+    public AddressBook(){ //constructor
+        BuddyList = new ArrayList<BuddyInfo>();
+    }
+    public void addBuddy(BuddyInfo Buddy){ //method to add buddy
+        BuddyList.add(Buddy);
+    }
+    public void removeBuddy(BuddyInfo Buddy){ //method to remove buddy
+        BuddyList.remove(Buddy);
+    }
+    public static void main(String[] args){
+        System.out.println("AddressBook");
+        BuddyInfo buddy= new BuddyInfo("Jad", "Carleton", "6137203334");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+
+    }
+}
